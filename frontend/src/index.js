d5249601a3b399fb136a6fdace2c70a5dcc1fb02
martabilbao <marta@bilbaoareitio.es>
2023-03-31 14:38:49 +0200

    Fix typo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { GlobalStyle } from './Styles/Globalstyles.styles'
+import GlobalStyle from './Styles/Globalstyles.styles'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
