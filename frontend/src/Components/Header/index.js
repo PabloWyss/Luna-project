224d7d21c238994f0f23476fad246d03e05384c9
Pablo Wyss <pabloawyss@gmail.com>
2023-03-31 16:47:45 +0200

    header style added

--- frontend/src/Components/Header/index.js
+++ frontend/src/Components/Header/index.js
@@ -1,9 +1,44 @@
+import {
+    HeaderButtonLeft, HeaderButtonRight,
+    HeaderButtonsDiv,
+    HeaderDiv,
+    HeaderLeftDiv,
+    HeaderLinksDiv,
+    HeaderRightDiv
+} from "./Header.style";
+import logo from "../../Assets/logo.svg"
+
 const Header = () => {
 
     return (
-        <p>
-            Hello Header
-        </p>
+        <HeaderDiv>
+            <HeaderLeftDiv >
+                <h1>
+                    <img src={logo}/>
+                </h1>
+            </HeaderLeftDiv >
+            <HeaderRightDiv>
+                <HeaderLinksDiv>
+                    <h2>
+                        <a>Home</a>
+                    </h2>
+                    <h2>
+                        <a>Search</a>
+                    </h2>
+                    <h2>
+                        <a>Profile</a>
+                    </h2>
+                </HeaderLinksDiv>
+                <HeaderButtonsDiv>
+                    <HeaderButtonLeft>
+                        SIGNUP
+                    </HeaderButtonLeft>
+                    <HeaderButtonRight>
+                        LOGIN
+                    </HeaderButtonRight>
+                </HeaderButtonsDiv>
+            </HeaderRightDiv>
+        </HeaderDiv>
     );
 };
 
