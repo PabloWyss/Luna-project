66c0e38578bf2c086838e8f9a4841cb4f0ff3e79
Pablo Wyss <pabloawyss@gmail.com>
2023-03-31 15:21:10 +0200

    Restaruants, registration and userpfile paths added

---
+++ frontend/src/Pages/Registration/index.js
@@ -0,0 +1,13 @@
+import Header from "../../Components/Header";
+
+const Registration = () => {
+    return (
+        <div>
+            <Header></Header>
+            <div></div>
+            <div></div>
+        </div>
+    )
+
+}
+export default Registration;
