18e9d7effdb2b9e9636895ef91dd986881b6a2aa
martabilbao <marta@bilbaoareitio.es>
2023-04-03 13:38:51 +0200

    Add Review component for Restaurant Page

---
+++ frontend/src/Pages/Restaurant/Review/Review.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const ReviewContainer = styled.div`
+  background-color: white;
+`
+
+const Review = () => {
+  return (
+    <div>
+      <div>
+        Header
+      </div>
+      <div>
+        Body
+      </div>
+    </div>
+  );
+}
+
+export default Review;
