b98b42d94a9bbb0e81e214272c021f19556ed0b3
martabilbao <marta@bilbaoareitio.es>
2023-04-03 14:31:59 +0200

    Create LikeCommentButtons component

---
+++ frontend/src/Components/LikeCommentButtons/LikeCommentButtonsStyles.js
@@ -0,0 +1,31 @@
+import styled from "styled-components"
+
+export const Buttons = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  border-radius: 28px;
+  color: #FFFFFF;
+  background-color: rgba(145, 145, 145, 0.6);
+  margin: 8px 0 17px 0;
+  font-size: 16px;
+  font-weight: 400;
+`
+
+export const LikeButton = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  border-right: 1px solid #FFFFFF;
+  padding: 13px;
+  gap: 11px;
+`
+
+export const CommentButton = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 13px;
+  gap: 11px;
+`
--- frontend/src/Pages/Search/ReviewsGrid/ReviewCard/ReviewCard.js
+++ frontend/src/Pages/Search/ReviewsGrid/ReviewCard/ReviewCard.js
@@ -1,6 +1,6 @@
-import avatar from "../../../../Assets/temp/JohnSmith.jpeg"
-import { FaRegThumbsUp } from 'react-icons/fa'
-import { Buttons, Card, CardHeader, Comment, CommentButton, LatestComments, LikeButton, CardBody, TextGreyBold, TextOrangeBig, TextOrangeSmall, UserAvatar, UserInfo } from "./ReviewCardStyles";
+import avatar from "../../../../Assets/temp/JohnSmith.jpeg";
+import { Card, CardHeader, Comment, LatestComments, CardBody, TextGreyBold, TextOrangeBig, TextOrangeSmall, UserAvatar, UserInfo } from "./ReviewCardStyles";
+import LikeCommentButtons from "../../../../Components/LikeCommentButtons/LikeCommentButtons";
 
 const ReviewCard = () => {
   return (
@@ -18,17 +18,7 @@ const ReviewCard = () => {
         <TextOrangeBig>Restaurant name</TextOrangeBig>
         <TextGreyBold>Went there for the first time, we have a very spicy chicken wings Alabama style, I have to say I was very impressed about the quality of the chicken, the place looks great but...s</TextGreyBold>
         <TextOrangeSmall>read more</TextOrangeSmall>
-        <Buttons>
-          <LikeButton>
-            <FaRegThumbsUp></FaRegThumbsUp>
-            <p>Like</p>
-            <p>63</p>
-          </LikeButton>
-          <CommentButton>
-            <p>Comment</p>
-            <p>24</p>
-          </CommentButton>
-        </Buttons>
+        <LikeCommentButtons />
         <LatestComments>Latest comments</LatestComments>
         <TextOrangeSmall>Name</TextOrangeSmall>
         <Comment>Is good! but too expensive!</Comment>
