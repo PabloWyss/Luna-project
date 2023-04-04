71cd16425695d3df7bec887d80ff563d2b3df75e
martabilbao <marta@bilbaoareitio.es>
2023-04-04 18:57:42 +0200

    Show 4 restaurants

--- frontend/src/Pages/Homepage/Homepage.js
+++ frontend/src/Pages/Homepage/Homepage.js
@@ -1,7 +1,30 @@
-import RestaurantCard from "../Search/RestaurantsGrid/RestaurantCard/RestaurantCard";
 import { Grid, HomeBanner, HomeBody, Main, Underline } from "./HomepageStyles";
+import RestaurantsGrid from '../Search/RestaurantsGrid/RestaurantsGrid';
+import { useEffect, useState } from "react";
+import lunaAPI from "../../Axios/lunaApi";
+import RestaurantCard from "../Search/RestaurantsGrid/RestaurantCard/RestaurantCard";
 
 const Homepage = () => {
+
+  const [listOfRestaurants, setListOfRestaurants] = useState([])
+
+  useEffect(() => {
+    const getFourBestRatedRestaurants = async () => {
+      try {
+        const response = await lunaAPI.get(`/restaurants/`)
+        setListOfRestaurants(response.data)
+      }
+      catch (error) {
+        console.log(error)
+      }
+    }
+    getFourBestRatedRestaurants();
+  }, [])
+
+  const sortedList = listOfRestaurants?.sort((a, b) => b.rating - a.rating);
+  const topFourList = sortedList?.slice(0, 4);
+  console.log(topFourList);
+
   return (
     <Main>
       <HomeBanner>
@@ -11,10 +34,11 @@ const Homepage = () => {
         <p>Best rated restaurants</p>
         <Underline></Underline>
         <Grid>
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
+          {
+            topFourList?.map(restaurant => {
+              return <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+            })
+          }
         </Grid>
       </HomeBody>
     </Main >
