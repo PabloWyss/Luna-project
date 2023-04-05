157f761bcb363cc784f5e1f28dbf9b1320f697b8
5aleG <sascha@digicy.ch>
2023-04-05 10:54:26 +0200

    Review and Comment serializer adjustments

--- frontend/src/Pages/AddReview/AddReviewStyles.js
+++ frontend/src/Pages/AddReview/AddReviewStyles.js
@@ -1,11 +1,39 @@
 import styled from 'styled-components';
 
+export const ParentContainer = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
+`
+
 export const HeaderContainer = styled.div`
-    height: 500px;
-    weight: 1440px;
-    margin-top: 100px;
-    color: black;
-    background: #F2F2F2;
-    position: absolute;
+display: flex;
+justify-content: center;
+align-items: center;
+margin: 0;
+background: black;
+height: 204px;
+`
+
+export const BackgroundImage = styled.img`
+min-width: 100%;
+object-fit: cover;
+height: 40vh;
+`
+
+// the form coniainer should be in the middle having a width: 832px; height: 431px;v left: 304px; top: 301px;
+
+export const FormContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+width: 832px;
+height: 431px;
+background: blue;
+border: 1px solid #000000;
+box-sizing: border-box;
+border-radius: 3px;
 `
 
