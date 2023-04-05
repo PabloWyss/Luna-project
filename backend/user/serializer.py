06f4f0449e51f2489aa18205531acca8bd82bc7c
5aleG <sascha@digicy.ch>
2023-04-05 09:59:53 +0200

    Review and Comment serializer adjustments

--- backend/user/serializer.py
+++ backend/user/serializer.py
@@ -8,7 +8,7 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        exclude = ('password', 'is_staff', 'is_active', 'date_joined', 'is_superuser', 'user_permissions')
+        exclude = ('password', 'is_staff', 'is_active', 'date_joined', 'is_superuser', 'user_permissions', 'groups')
 
 
 class UserUpdateSerializer(serializers.ModelSerializer):
