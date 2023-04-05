6cfd22a2a3748ed3603eca35bd4f3af15ddfc8b7
Hatice Anik <haticeanik12@gmail.com>
2023-04-05 12:37:52 +0200

    Profile: Correct fetch & Backend update via Edit, Avatar also connected to edit, shows up in usergrid too, started working on reviews and comments in Profile

--- frontend/src/Pages/Search/UsersGrid/UserCard/UserCard.js
+++ frontend/src/Pages/Search/UsersGrid/UserCard/UserCard.js
@@ -1,6 +1,8 @@
 import avatar from "../../../../Assets/temp/JohnSmith.jpeg"
 import { Card, CardHeader, CardBody, TextGreyBold, TextOrangeBig, TextOrangeSmall, UserAvatar, UserInfo } from "./UserCardStyles";
 
+const isDev = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development');
+let baseUrl = isDev ? 'http://localhost:8001' : 'https://luna-team4.propulsion-learn.ch'
 const UserCard = (props) => {
 
 
@@ -8,7 +10,7 @@ const UserCard = (props) => {
     <Card>
       <CardHeader>
         <UserAvatar>
-          <img src={props.user.avatar}></img>
+          <img src={baseUrl + props.user.avatar}></img>
         </UserAvatar>
         <UserInfo>
           <TextOrangeBig>{props.user.username}</TextOrangeBig>
