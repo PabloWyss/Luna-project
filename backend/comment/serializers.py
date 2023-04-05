9cf37b1ed68087301290cea7a46ccd5a4c636d94
5aleG <sascha@digicy.ch>
2023-04-05 10:36:56 +0200

    Review and Comment serializer adjustments

--- backend/comment/serializers.py
+++ backend/comment/serializers.py
@@ -13,12 +13,10 @@ class CommentSerializer(serializers.ModelSerializer):
 
 
 class CreateCommentSerializer(serializers.ModelSerializer):
-    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    review = serializers.PrimaryKeyRelatedField(queryset=RestaurantReview.objects.all())
 
     class Meta:
         model = Comment
-        fields = ['text_content', 'user', 'review']
+        fields = ['text_content']
 
 
 class DeleteSerializer(serializers.ModelSerializer):
