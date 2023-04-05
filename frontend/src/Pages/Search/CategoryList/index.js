c43edaecce8b30a86152e436569c94f53cc4c13f
Pablo Wyss <pabloawyss@gmail.com>
2023-04-05 13:51:38 +0200

    review error corrected

---
+++ frontend/src/Pages/Search/CategoryList/index.js
@@ -0,0 +1,17 @@
+import {CategoryUL} from "./CategoryList.styles";
+
+const CategoryList = () =>{
+    return (
+        <CategoryUL >
+            <li>CH</li>
+            <li>US</li>
+            <li>IT</li>
+            <li>SP</li>
+            <li>KR</li>
+            <li>TH</li>
+            <li>SRB</li>
+        </CategoryUL >
+    )
+}
+
+export default CategoryList
