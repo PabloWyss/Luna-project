1d99e79debf1e2c4a97c664f6252e9ec6c97e9ee
martabilbao <marta@bilbaoareitio.es>
2023-04-05 15:39:02 +0200

    Implement search functionality homepage

--- frontend/src/Pages/Homepage/HomepageStyles.js
+++ frontend/src/Pages/Homepage/HomepageStyles.js
@@ -13,15 +13,17 @@ export const HomeBanner = styled.div`
   height: 350px;
   background-size: cover;
   background-repeat: no-repeat;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
+
+  >div{
+    width: fit-content;
+    margin: auto;
+  }
 `
 
 export const SearchWraper = styled.div`
   display: flex;
   flex-direction: row;
+  margin-top: 148px;
 
   input{
     padding: 20px 15px;
@@ -29,14 +31,48 @@ export const SearchWraper = styled.div`
     border: 1px solid #EBEBEB;
     border-radius: 3px;
     margin-right: 20px;
-    font-size: 16px;
-    font-weight: 400;
+    font-size: 20px;
+    font-weight: 300;
+    outline: black
   }
 `
 
 export const ButtonWraper = styled.div`
   width: 200px;
   flex-shrink: 0;
+`
+
+export const SearchResults = styled.div`
+  background-color: white;
+  margin-top: 5px;
+  width: 532px;
+  border-radius: 3px;
+`
+
+export const SearchedRestaurant = styled.div`
+  width: 532px;
+  padding: 10px 15px;
+  font-size: 20px;
+  font-weight: 400;
+  border-bottom: 1px solid #EBEBEB;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  :hover{
+    cursor: pointer;
+    background-color: #EBEBEB;
+  }
+`
+export const Name = styled.div`
+  font-size: 20px;
+  font-weight: 400;
+  margin-bottom: 5px;
+`
+
+export const Street = styled.div`
+  font-size: 16px;
+  font-weight: 300;
 `
 
 export const HomeBody = styled.div`
