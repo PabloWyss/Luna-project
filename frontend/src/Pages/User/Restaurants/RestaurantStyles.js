39da1400518f2f9f53cc182c16973be7824e8384
Hatice Anik <haticeanik12@gmail.com>
2023-04-05 15:53:28 +0200

    Create Restaurant Button redirects

--- frontend/src/Pages/User/Restaurants/RestaurantStyles.js
+++ frontend/src/Pages/User/Restaurants/RestaurantStyles.js
@@ -3,25 +3,26 @@ import RestaurantsComponent from "./RestaurantsComponent";
 import {Title, Container, ReviewsTitleDateDiv} from "../Reviews/ReviewStyles";
 import OrangeButton from "../../../Components/Button";
 import {RestaurantButtonDiv} from "./RestaurantsComponent/RestaurantComponent.style";
+import { NavLink } from 'react-router-dom';
 
 const Restaurant = () => {
   return (
-      <div>
-        <Container>
-            <ReviewsTitleDateDiv>
-                <Title>Restaurants</Title>
-            </ReviewsTitleDateDiv>
-            <RestaurantsComponent/>
-            <RestaurantsComponent/>
-            <RestaurantButtonDiv >
-              <OrangeButton textInput={'Create Restaurant'}/>
-            </RestaurantButtonDiv >
-        </Container>
-
-      </div>
-      );
-
-
+    <div>
+      <Container>
+        <ReviewsTitleDateDiv>
+          <Title>Restaurants</Title>
+        </ReviewsTitleDateDiv>
+        <RestaurantsComponent />
+        <RestaurantsComponent />
+        <RestaurantButtonDiv>
+          <NavLink to="/restaurants/new/">
+            <OrangeButton textInput={'Create Restaurant'} />
+          </NavLink>
+        </RestaurantButtonDiv>
+      </Container>
+    </div>
+  );
 };
+
 
 export default Restaurant;
