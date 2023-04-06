e636b71cd59f2f26bfad2b6284c021d0d232d136
martabilbao <marta@bilbaoareitio.es>
2023-04-06 12:22:10 +0200

    Implement filter search bar in reviews section

--- frontend/src/Pages/Restaurant/ReviewsList/ReviewsList.js
+++ frontend/src/Pages/Restaurant/ReviewsList/ReviewsList.js
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
 import styled from "styled-components";
-import lunaAPI from "../../../Axios/lunaApi";
 import Review from '../ReviewsList/Review/Review'
 
 const List = styled.div`
@@ -9,36 +7,12 @@ const List = styled.div`
   gap: 15px; 
 `
 
-const ReviewsList = ({ restaurantID }) => {
-  const [reviewsList, setReviewsList] = useState([])
-
-  useEffect(() => {
-    if (!localStorage.getItem('token')) {
-      return;
-    }
-
-    const getReviewsFromRestaurantByID = async () => {
-      try {
-        const config = {
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${localStorage.getItem('token')}`,
-          },
-        };
-
-        const response = await lunaAPI.get(`reviews/restaurant/${restaurantID}/`, config);
-        setReviewsList(response.data)
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    getReviewsFromRestaurantByID();
-  }, []);
-
+const ReviewsList = ({ reviews }) => {
+  console.log(reviews)
   return (
     <List>
       {
-        reviewsList.map(review => {
+        reviews?.map(review => {
           return <Review key={review.id} review={review} />
         })
       }
